6e205caaffc665d8d525ae9ae3f3a26164dba9b1
lljackie <782361105@qq.com>
2016-09-28 16:11:34 +0800

    Initial commit

---
+++ app/src/main/java/com/lljackie/test22_contacts/MainActivity.java
@@ -0,0 +1,62 @@
+package com.lljackie.test22_contacts;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG="Test22_Tag";
+    private ContentResolver resolver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resolver = this.getContentResolver();
+
+        Button bt_all = (Button) findViewById(R.id.bt_all);
+        bt_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor cursor=resolver.query(ContactsContract.Contacts.CONTENT_URI,null,null,null,null);
+                while(cursor.moveToNext()){
+
+                    String msg;
+                    //id
+                    String id=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    msg="id:"+id;
+
+                    //name
+                    String name=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    msg=msg+" name:"+name;
+
+                    //phone
+                    Cursor phoneNumbers=resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,ContactsContract.CommonDataKinds.Phone.CONTACT_ID +"="+id,null,null);
+                    while(phoneNumbers.moveToNext()){
+                        String phoneNumber=phoneNumbers.getString(phoneNumbers.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        msg=msg+" phone:"+phoneNumber;
+                    }
+
+                    //email
+                    Cursor emails=resolver.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,null,ContactsContract.CommonDataKinds.Email.CONTACT_ID +"="+id,null,null);
+                    while(emails.moveToNext()){
+                        String email=emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                        msg=msg+" email:"+email;
+                    }
+
+                    Log.e(TAG,msg);
+
+
+
+                }
+
+            }
+        });
+    }
+}
